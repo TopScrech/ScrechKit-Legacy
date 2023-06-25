c1e5b304494f7a37387f3f6bdeb760ce6dfa7ae3
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-06-25 08:35:15 +0200

    -

--- Sources/ScrechKit/formatDate.swift
+++ Sources/ScrechKit/formatDate.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+@available(iOS 16, macOS 13, tvOS 16, watchOS 9, *)
 public func timeSinceISO(_ date: String) -> LocalizedStringResource {
     let formatter = ISO8601DateFormatter()
     guard let date = formatter.date(from: date) else { return "-" }
