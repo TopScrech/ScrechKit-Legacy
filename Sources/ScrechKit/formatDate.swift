035979c017454f955ad33d56bf12005fad1c98a4
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-06-25 09:02:57 +0200

    -

--- Sources/ScrechKit/formatDate.swift
+++ Sources/ScrechKit/formatDate.swift
@@ -19,15 +19,7 @@ public func timeSinceISO(_ date: String) -> LocalizedStringResource {
     return "\(sinceNowDays) days ago"
 }
 
-public func formatDate(_ date: String) -> Date? {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-    formatter.locale = Locale(identifier: "en_US_POSIX")
-    
-    return formatter.date(from: date)
-}
-
-public func formatDateToString(_ date: String) -> String {
+public func formatISO(_ date: String) -> String {
     let formatter = DateFormatter()
     formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
     formatter.locale = Locale(identifier: "en_US_POSIX")
@@ -38,15 +30,4 @@ public func formatDateToString(_ date: String) -> String {
     
     formatter.dateFormat = "dd-MM-yyyy"
     return formatter.string(from: date)
-}
-
-public func numberOfDaysBetween(from: Date, to: Date = Date()) -> Int {
-    let calendar = Calendar.current
-    let numberOfDays = calendar.dateComponents([.day], from: from, to: to)
-    
-    if let days = numberOfDays.day {
-        return days
-    } else {
-        return 0
-    }
 }
