de5e4147b123b11ec97ead9dba54d6db58f8d780
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-06-27 07:01:59 +0200

    -

--- Sources/ScrechKit/UI Elements/MenuButton.swift
+++ Sources/ScrechKit/UI Elements/MenuButton.swift
@@ -2,24 +2,28 @@ import SwiftUI
 
 @available(iOS 14, macOS 11, tvOS 14, watchOS 7, *)
 public struct MenuButton: View {
-    let name, icon: String
-    let action: () -> Void
+    private let name: LocalizedStringResource
+    private let role: ButtonRole
+    private let icon: String
+    private let action: () -> Void
     
     public init (
-        _ name: String,
+        _ name: LocalizedStringResource,
+        role: ButtonRole = .cancel,
         icon: String,
         action: @escaping () -> Void
     ) {
         self.name = name
+        self.role = role
         self.icon = icon
         self.action = action
     }
     
     public var body: some View {
-        Button {
+        Button(role: role) {
             action()
         } label: {
-            Label(name, systemImage: icon)
+            Label("\(name)", systemImage: icon)
         }
     }
 }
